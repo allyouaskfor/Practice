d1d4643cea99f3ad28da51042e7a6dce374510e3
Ethan Priest <allyouaskfor@users.noreply.github.com>
2019-09-30 12:08:04 -0600

    sync
    updated with progress from iPad.

---
+++ 3js_learn/README.md
@@ -0,0 +1,2 @@
+## THREE.js
+This repository is used to keep my progress of learning threejs synced between my devices.
--- 3js_learn/js/scene.js
+++ 3js_learn/js/scene.js
@@ -1,41 +1,32 @@
+/*
+# Author: allyouaskfor, canibeanartist
+# Github: https://github.com/allyouaskfor/Practice/3js_learn
+# 
+*/
+
 // Declaration
 let scene;
 let camera;
+let controls;
+let controlsO;
 let renderer;
 let mesh;
 
 const AMOUNT = 6;
 
-const bannerTjs = document.querySelector('#three-canv');
+const banner = document.querySelector('#three-canv');
 
 function init() {
 
     scene = new THREE.Scene();
 
-    // Camera
-    camera = new THREE.PerspectiveCamera(
-        20,
-        window.innerWidth / window.innerHeight,
-//        bannerTjs.clientWidth / bannerTjs.clientHeight, 
-        0.1,
-        100
-    );
-    camera.position.set( 0, 0, 10 );
-
-    // Renderer
-    renderer = new THREE.WebGLRenderer( {
-        alpha: true,
-        antialias: true
-    } );
-    
-    renderer.setSize( window.innerWidth, window.innerHeight );
-//    renderer.setSize( bannerTjs.clientWidth, bannerTjs.clientHeight );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    
-    renderer.gammaFactor = 2.2;
-    renderer.gammaOutput = true;
-    
-    document.body.appendChild( renderer.domElement );
+    createCamera();
+//    createControlsTrack();
+    createControlsOrbit();
+    createLights();
+    createPlane();
+    createMeshes();
+    createRenderer();
     
     renderer.setAnimationLoop( () => {
 
@@ -44,7 +35,74 @@ function init() {
 
     } );
 
-    // Object
+}
+    
+function createCamera() {
+
+    camera = new THREE.PerspectiveCamera(
+        20,
+//        window.innerWidth / window.innerHeight,
+        banner.clientWidth / banner.clientHeight, 
+        0.1,
+        1000
+
+    );
+
+//    camera.position.set( 0, 0, 10 );
+    camera.position.set( -4, 4, 10 );
+
+}
+
+function createControlsTrack() {
+    controls = new THREE.TrackballControls( camera );
+    controls.rotateSpeed = 1.0;
+    controls.zoomSpeed = 1.0;
+    controls.panSpeed = 1.0;
+//    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.01;
+}
+
+function createControlsOrbit() {
+    controlsO = new THREE.OrbitControls( camera );
+}
+
+function createLights() {
+
+    const ambLight = new THREE.HemisphereLight(
+        0x9dceff,   // Bright sky colour
+        0x404050,   // Dim ground colour
+        5,          // Intensity
+    );
+    scene.add( ambLight );
+
+    const dirLight = new THREE.DirectionalLight();
+    dirLight.intensity = 5;
+    dirLight.position.set( 1, 1, 0 );
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.zoom = 1;
+
+    scene.add( dirLight );
+
+}
+
+function createPlane() {
+
+    let planeGeometry = new THREE.PlaneGeometry( 10, 6, 20, 20 );
+    let planeMaterial = new THREE.MeshPhongMaterial( { color: 0xbbaafe } );
+    let plane = new THREE.Mesh( planeGeometry, planeMaterial );
+    plane.receiveShadow = true;
+
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 0;
+    plane.position.y = -2;
+    plane.position.z = 0;
+
+    scene.add( plane );
+
+}
+
+function createMeshes() {
+
     geometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
     const textureLoader = new THREE.TextureLoader();
     const texture = textureLoader.load( 'textures/uv_map_basic.png' );
@@ -52,43 +110,47 @@ function init() {
     texture.anisotropy = 16;
     material = new THREE.MeshStandardMaterial( {
         color: 0xa64696,
-        map: texture,
+//        map: texture,
     } );
     
     mesh = new THREE.Mesh( geometry, material );
     mesh.castShadow = true;
     mesh.receiveShadow = true;
     scene.add( mesh );
+    
+}
 
-    // Lights
-    const ambLight = new THREE.AmbientLight( {
-        color: 0xf2ec91,
-        intensity: 1
+function createRenderer() {
+
+    renderer = new THREE.WebGLRenderer( {
+        alpha: true,
+        antialias: true
     } );
     
-    ambLight.position.set( 0, 1, 0 );
+//    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( banner.clientWidth, banner.clientHeight );
+    renderer.setPixelRatio( window.devicePixelRatio );
     
-    scene.add( ambLight );
-
-    const dirLight = new THREE.DirectionalLight();
-    dirLight.position.set( 0.5, 0.5, 1 );
-    dirLight.castShadow = true;
-    dirLight.shadow.camera.zoom = 4;
-
-    scene.add( dirLight );
+    renderer.gammaFactor = 2.2;
+    renderer.gammaOutput = true;
+    
+    renderer.physicallyCorrectLights = true;
+    
+    document.body.appendChild( renderer.domElement );
 
 }
 
 function update() {
-
+/*
     mesh.rotation.x += 0.005;
     mesh.rotation.y += 0.005;
     mesh.rotation.z += 0.01;
-
+*/
 }
 
 function render() {
 
+//    controls.update();
     renderer.render( scene, camera );
 
 }
@@ -97,11 +159,11 @@ function onWindowResize() {
     
     // console.log( 'Window resized.' );
     
-    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.aspect = banner.clientWidth / banner.clientHeight;
     
     camera.updateProjectionMatrix();
     
-    renderer.setSize( container.clientWidth, container.clientHeight );
+    renderer.setSize( banner.clientWidth, banner.clientHeight );
     
 }
 
